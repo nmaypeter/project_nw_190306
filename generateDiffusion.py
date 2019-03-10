37dcb9b947ad000f4cc386d7ab3670d4aa8eaa9b
nmaypeter <37822464+nmaypeter@users.noreply.github.com>
2019-03-11 02:09:19 +0800

    Update generateDiffusion.py

--- generateDiffusion.py
+++ generateDiffusion.py
@@ -27,24 +27,24 @@ if __name__ == '__main__':
 
     for pk in range(len(price_list_g)):
         mu_g = np.mean(price_list_g[pk])
-        # mu = sum(price_list[pk])
+        # mu_g = sum(price_list[pk])
         sigma_g = (max(price_list_g[pk]) - mu_g) / 0.8415
-        # sigma = 1
-        X = np.arange(0, 2, 0.001)
+        # sigma_g = 1
+        X_g = np.arange(0, 2, 0.001)
 
-        Y = [stats.norm.pdf(X, mu_g, sigma_g), stats.norm.cdf(X, mu_g, sigma_g), stats.norm.sf(X, mu_g, sigma_g)]
+        Y_g = [stats.norm.pdf(X_g, mu_g, sigma_g), stats.norm.cdf(X_g, mu_g, sigma_g), stats.norm.sf(X_g, mu_g, sigma_g)]
         X_label = ['wallet guess', 'wallet guess', 'number of nodes with purchasing ability guess']
         Y_label = ['probability density', 'probability', 'probability']
         title = ['pdf', 'cdf', 'ccdf']
-        for index in range(len(Y)):
-            plt.plot(X, Y[index])
+        for index in range(len(Y_g)):
+            plt.plot(X_g, Y_g[index])
             plt.xlabel(X_label[index])
             plt.ylabel(Y_label[index])
             plt.title(title[index] + ' of normal distribution: μ = ' + str(mu_g) + ', σ = ' + str(round(sigma_g, 4)))
             plt.grid()
             plt.show()
 
-        pw_list = [round(float(Y[2][np.argwhere(X == p)]), 4) for p in price_list_g[pk]]
+        pw_list = [round(float(Y_g[2][np.argwhere(X_g == p)]), 4) for p in price_list_g[pk]]
         print(pw_list)
         print(round(float(stats.norm.sf(price_list_g[pk][0] + price_list_g[pk][1], mu_g, sigma_g)), 4))
         print(round(float(stats.norm.sf(price_list_g[pk][0] + price_list_g[pk][2], mu_g, sigma_g)), 4))
