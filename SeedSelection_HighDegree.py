55054e552fc57ab3960fcdafd6f492f1acc3027c
nmaypeter <37822464+nmaypeter@users.noreply.github.com>
2019-03-15 01:33:58 +0800

    Update SeedSelection_HighDegree.py
    1. 取消ss的budget參數，將超過預算判斷移出。

--- SeedSelection_HighDegree.py
+++ SeedSelection_HighDegree.py
@@ -2,17 +2,15 @@ from Diffusion_NormalIC import *
 
 
 class SeedSelectionHD:
-    def __init__(self, g_dict, s_c_dict, prod_list, total_bud):
+    def __init__(self, g_dict, s_c_dict, prod_list):
         ### g_dict: (dict) the graph
         ### s_c_dict: (dict) the set of cost for seeds
         ### prod_list: (list) the set to record products [kk's profit, kk's cost, kk's price]
-        ### total_bud: (int) the budget to select seed
         ### num_node: (int) the number of nodes
         ### num_product: (int) the kinds of products
         self.graph_dict = g_dict
         self.seed_cost_dict = s_c_dict
         self.product_list = prod_list
-        self.total_budget = total_bud
         self.num_node = len(s_c_dict)
         self.num_product = len(prod_list)
 
@@ -59,7 +57,8 @@ class SeedSelectionHD:
 
         return d_dict
 
-    def getHighDegreeNode(self, d_dict, cur_bud):
+    @staticmethod
+    def getHighDegreeNode(d_dict):
         # -- get the node with highest degree --
         mep = [-1, '-1']
         max_degree = -1
@@ -108,20 +107,20 @@ if __name__ == '__main__':
 
     # -- initialization for each budget --
     start_time = time.time()
-    sshd = SeedSelectionHD(graph_dict, seed_cost_dict, product_list, total_budget)
+    sshd = SeedSelectionHD(graph_dict, seed_cost_dict, product_list)
 
     # -- initialization for each sample_number --
     now_budget = 0.0
     seed_set = [set() for _ in range(num_product)]
 
     degree_dict = sshd.constructDegreeDict(data_set_name)
-    mep_g, degree_dict = sshd.getHighDegreeNode(degree_dict, now_budget)
+    mep_g, degree_dict = sshd.getHighDegreeNode(degree_dict)
     mep_k_prod, mep_i_node = mep_g[0], mep_g[1]
 
     # -- main --
     while now_budget < total_budget and mep_i_node != '-1':
         if now_budget + seed_cost_dict[mep_i_node] > total_budget:
-            mep_g, degree_dict = sshd.getHighDegreeNode(degree_dict, now_budget)
+            mep_g, degree_dict = sshd.getHighDegreeNode(degree_dict)
             mep_k_prod, mep_i_node = mep_g[0], mep_g[1]
             if mep_i_node == '-1':
                 break
@@ -130,7 +129,7 @@ if __name__ == '__main__':
         seed_set[mep_k_prod].add(mep_i_node)
         now_budget += seed_cost_dict[mep_i_node]
 
-        mep_g, degree_dict = sshd.getHighDegreeNode(degree_dict, now_budget)
+        mep_g, degree_dict = sshd.getHighDegreeNode(degree_dict)
         mep_k_prod, mep_i_node = mep_g[0], mep_g[1]
 
     eva = Evaluation(graph_dict, seed_cost_dict, product_list, pp_strategy, whether_passing_information_without_purchasing)
