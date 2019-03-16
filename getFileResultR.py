2d36f26ce352dae0db93e86539272fe75f36108d
nmaypeter <37822464+nmaypeter@users.noreply.github.com>
2019-03-17 02:44:12 +0800

    Update getFileResultR.py
    1. 將不同pps的結果也放入同一txt中。

--- getFileResultR.py
+++ getFileResultR.py
@@ -7,17 +7,18 @@ pps_seq = [1, 2, 3]
 wpiwp_seq = [bool(0), bool(1)]
 prod_setting_seq, prod_setting2_seq = [1, 2], [1, 2, 3]
 dis_seq = [1, 2]
+
 for data_setting in data_setting_seq:
     data_set_name = 'email_undirected' * (data_setting == 1) + 'dnc_email_directed' * (data_setting == 2) + 'email_Eu_core_directed' * (data_setting == 3) + \
                     'WikiVote_directed' * (data_setting == 4) + 'NetPHY_undirected' * (data_setting == 5)
     for m in model_seq:
         model_name = 'mngic' * (m == 1) + 'mhdic' * (m == 2) + 'mric' * (m == 3) + 'mpmisic' * (m == 4) + \
-                     'mngscsic' * (m == 5) + 'mngric' * (m == 6) + 'mngpwic' * (m == 7) + \
+                     'mngric' * (m == 5) + 'mngpwic' * (m == 6) + 'mngrpwic' * (m == 7) + \
                      'mhedic' * (m == 8) + 'mhdpwic' * (m == 9) + 'mhedpwic' * (m == 10) + '_pps'
-        for pps in pps_seq:
-            profit, cost, time_avg, time_total = [], [], [], []
-            ratio_profit, ratio_cost, number_an, number_seed = [], [], [], []
-            for dis in dis_seq:
+        for dis in dis_seq:
+            profit, cost, time_avg, time_total = [[] for _ in range(len(pps_seq))], [[] for _ in range(len(pps_seq))], [[] for _ in range(len(pps_seq))], [[] for _ in range(len(pps_seq))]
+            ratio_profit, ratio_cost, number_an, number_seed = [[] for _ in range(len(pps_seq))], [[] for _ in range(len(pps_seq))], [[] for _ in range(len(pps_seq))], [[] for _ in range(len(pps_seq))]
+            for pps in pps_seq:
                 for prod_setting in prod_setting_seq:
                     for wpiwp in wpiwp_seq:
                         for prod_setting2 in prod_setting2_seq:
@@ -28,25 +29,25 @@ for data_setting in data_setting_seq:
 
                                 with open(result_name + '/1profit.txt') as f:
                                     for line in f:
-                                        profit.append(line)
+                                        profit[pps - 1].append(line)
                                 f.close()
                                 with open(result_name + '/2cost.txt') as f:
                                     for line in f:
-                                        cost.append(line)
+                                        cost[pps - 1].append(line)
                                 f.close()
                                 with open(result_name + '/3time_avg.txt') as f:
                                     for line in f:
-                                        time_avg.append(line)
+                                        time_avg[pps - 1].append(line)
                                 f.close()
                                 with open(result_name + '/4time_total.txt') as f:
                                     for line in f:
-                                        time_total.append(line)
+                                        time_total[pps - 1].append(line)
                                 f.close()
                             except FileNotFoundError:
-                                profit.append('')
-                                cost.append('')
-                                time_total.append('')
-                                time_avg.append('')
+                                profit[pps - 1].append('')
+                                cost[pps - 1].append('')
+                                time_total[pps - 1].append('')
+                                time_avg[pps - 1].append('')
                                 continue
 
                     for prod_setting2 in [1, 2, 3]:
@@ -60,78 +61,80 @@ for data_setting in data_setting_seq:
 
                                 with open(result_name + '/5ratio_profit.txt') as f:
                                     for line in f:
-                                        ratio_profit.append(line)
+                                        ratio_profit[pps - 1].append(line)
                                 f.close()
                                 with open(result_name + '/6ratio_cost.txt') as f:
                                     for line in f:
-                                        ratio_cost.append(line)
+                                        ratio_cost[pps - 1].append(line)
                                 f.close()
                                 with open(result_name + '/7number_pn.txt') as f:
                                     for line in f:
-                                        number_an.append(line)
+                                        number_an[pps - 1].append(line)
                                 f.close()
                                 with open(result_name + '/8number_seed.txt') as f:
                                     for line in f:
-                                        number_seed.append(line)
+                                        number_seed[pps - 1].append(line)
                                 f.close()
                             except FileNotFoundError:
                                 for num in range(num_product):
-                                    ratio_profit.append('\n')
-                                    ratio_cost.append('\n')
-                                    number_seed.append('\n')
-                                    number_an.append('\n')
+                                    ratio_profit[pps - 1].append('\n')
+                                    ratio_cost[pps - 1].append('\n')
+                                    number_seed[pps - 1].append('\n')
+                                    number_an[pps - 1].append('\n')
                                 continue
 
+            for pps in pps_seq:
+                write_file = 'w' * (pps == 1) + 'a' * (pps != 1)
                 path1 = 'result/r_' + data_set_name + '/r_dis' + str(dis)
                 if not os.path.isdir(path1):
                     os.mkdir(path1)
-                path = 'result/r_' + data_set_name + '/r_dis' + str(dis) + '/' + model_name + str(pps)
-                fw = open(path + '_1profit.txt', 'w')
-                for lnum, line in enumerate(profit):
+                path = 'result/r_' + data_set_name + '/r_dis' + str(dis) + '/' + model_name.replace('_pps', '')
+                fw = open(path + '_1profit.txt', write_file)
+                for lnum, line in enumerate(profit[pps - 1]):
                     fw.write(str(line) + '\n')
-                    if lnum == 5:
+                    if lnum % 6 == 5:
                         fw.write('\n' * 10)
                 fw.close()
-                fw = open(path + '_2cost.txt', 'w')
-                for lnum, line in enumerate(cost):
+                fw = open(path + '_2cost.txt', write_file)
+                for lnum, line in enumerate(cost[pps - 1]):
                     fw.write(str(line) + '\n')
-                    if lnum == 5:
+                    if lnum % 6 == 5:
                         fw.write('\n' * 10)
                 fw.close()
-                fw = open(path + '_3time_avg.txt', 'w')
-                for lnum, line in enumerate(time_avg):
+                fw = open(path + '_3time_avg.txt', write_file)
+                for lnum, line in enumerate(time_avg[pps - 1]):
                     fw.write(str(line) + '\n')
-                    if lnum == 5:
+                    if lnum % 6 == 5:
                         fw.write('\n' * 10)
                 fw.close()
-                fw = open(path + '_4time_total.txt', 'w')
-                for lnum, line in enumerate(time_total):
+                fw = open(path + '_4time_total.txt', write_file)
+                for lnum, line in enumerate(time_total[pps - 1]):
                     fw.write(str(line) + '\n')
-                    if lnum == 5:
+                    if lnum % 6 == 5:
                         fw.write('\n' * 10)
                 fw.close()
 
-                fw = open(path + '_5ratio_profit.txt', 'w')
-                for lnum, line in enumerate(ratio_profit):
-                    if lnum % 6 == 0 and lnum != 0:
+                fw = open(path + '_5ratio_profit.txt', write_file)
+                for lnum, line in enumerate(ratio_profit[pps - 1]):
+                    if (lnum % 6 == 0 and lnum != 0 and pps == 1) or (lnum % 6 == 0 and pps != 1):
                         fw.write('\n' * 9)
                     fw.write(str(line))
                 fw.close()
-                fw = open(path + '_6ratio_cost.txt', 'w')
-                for lnum, line in enumerate(ratio_cost):
-                    if lnum % 6 == 0 and lnum != 0:
+                fw = open(path + '_6ratio_cost.txt', write_file)
+                for lnum, line in enumerate(ratio_cost[pps - 1]):
+                    if (lnum % 6 == 0 and lnum != 0 and pps == 1) or (lnum % 6 == 0 and pps != 1):
                         fw.write('\n' * 9)
                     fw.write(str(line))
                 fw.close()
-                fw = open(path + '_7number_pn.txt', 'w')
-                for lnum, line in enumerate(number_an):
-                    if lnum % 6 == 0 and lnum != 0:
+                fw = open(path + '_7number_pn.txt', write_file)
+                for lnum, line in enumerate(number_an[pps - 1]):
+                    if (lnum % 6 == 0 and lnum != 0 and pps == 1) or (lnum % 6 == 0 and pps != 1):
                         fw.write('\n' * 9)
                     fw.write(str(line))
                 fw.close()
-                fw = open(path + '_8number_seed.txt', 'w')
-                for lnum, line in enumerate(number_seed):
-                    if lnum % 6 == 0 and lnum != 0:
+                fw = open(path + '_8number_seed.txt', write_file)
+                for lnum, line in enumerate(number_seed[pps - 1]):
+                    if (lnum % 6 == 0 and lnum != 0 and pps == 1) or (lnum % 6 == 0 and pps != 1):
                         fw.write('\n' * 9)
                     fw.write(str(line))
                 fw.close()
