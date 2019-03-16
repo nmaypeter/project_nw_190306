ddee54858ebaf712e33087730b913329c35629e6
nmaypeter <37822464+nmaypeter@users.noreply.github.com>
2019-03-17 02:43:34 +0800

    Update getFileResult.py

--- getFileResult.py
+++ getFileResult.py
@@ -7,12 +7,13 @@ pps_seq = [1, 2, 3]
 wpiwp_seq = [bool(0), bool(1)]
 prod_setting_seq, prod_setting2_seq = [1, 2], [1, 2, 3]
 dis_seq = [1, 2]
+
 for data_setting in data_setting_seq:
     data_set_name = 'email_undirected' * (data_setting == 1) + 'dnc_email_directed' * (data_setting == 2) + 'email_Eu_core_directed' * (data_setting == 3) + \
                     'WikiVote_directed' * (data_setting == 4) + 'NetPHY_undirected' * (data_setting == 5)
     for m in model_seq:
         model_name = 'mngic' * (m == 1) + 'mhdic' * (m == 2) + 'mric' * (m == 3) + 'mpmisic' * (m == 4) + \
-                     'mngscsic' * (m == 5) + 'mngric' * (m == 6) + 'mngpwic' * (m == 7) + \
+                     'mngric' * (m == 5) + 'mngpwic' * (m == 6) + 'mngrpwic' * (m == 7) + \
                      'mhedic' * (m == 8) + 'mhdpwic' * (m == 9) + 'mhedpwic' * (m == 10) + '_pps'
         for pps in pps_seq:
             for wpiwp in wpiwp_seq:
