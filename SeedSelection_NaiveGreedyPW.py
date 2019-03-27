137959f3a5457b5aeeb44b8e18e913d16a379057
nmaypeter <37822464+nmaypeter@users.noreply.github.com>
2019-03-27 23:49:16 +0800

    Update SeedSelection_NaiveGreedyPW.py

--- SeedSelection_NaiveGreedyPW.py
+++ SeedSelection_NaiveGreedyPW.py
@@ -1,64 +1,50 @@
-from Diffusion_NormalIC import *
+from Diffusion import *
 
 
 class SeedSelectionNGPW:
-    def __init__(self, g_dict, s_c_dict, prod_list, dis, monte):
+    def __init__(self, g_dict, s_c_dict, prod_list, pw_list, monte):
         ### g_dict: (dict) the graph
         ### s_c_dict: (dict) the set of cost for seeds
         ### prod_list: (list) the set to record products [kk's profit, kk's cost, kk's price]
         ### num_node: (int) the number of nodes
         ### num_product: (int) the kinds of products
-        ### dis: (int) wallet distribution
+        ### pw_list: (list) the product weight list
         ### monte: (int) monte carlo times
         self.graph_dict = g_dict
         self.seed_cost_dict = s_c_dict
         self.product_list = prod_list
         self.num_node = len(s_c_dict)
         self.num_product = len(prod_list)
-        self.dis = dis
+        self.pw_list = pw_list
         self.monte = monte
 
-    def getProductWeight(self):
-        price_list = [k[2] for k in self.product_list]
-        mu, sigma = 0, 1
-        if self.dis == 1:
-            mu = np.mean(price_list)
-            sigma = (max(price_list) - mu) / 0.8415
-        elif self.dis == 2:
-            mu = sum(price_list)
-            sigma = abs(min(price_list) - mu) / 3
-        X = np.arange(0, 2, 0.001)
-        Y = stats.norm.sf(X, mu, sigma)
-        product_weight_list = [round(float(Y[np.argwhere(X == p)]), 4) for p in price_list]
-
-        return product_weight_list
-
-    def generateCelfSequence(self, p_w_list):
+    def generateCelfSequence(self):
         # -- calculate expected profit for all combinations of nodes and products --
         ### celf_ep: (list) [k_prod, i_node, mg, flag]
-        celf_seq = [[-1, '-1', 0.0, 0]]
+        celf_seq = [(-1, '-1', 0.0, 0)]
 
-        diffpw_ss = DiffusionPW(self.graph_dict, self.seed_cost_dict, self.product_list, self.monte, p_w_list)
+        diffpw_ss = DiffusionPW(self.graph_dict, self.seed_cost_dict, self.product_list, self.pw_list, self.monte)
 
         for i in set(self.graph_dict.keys()):
             s_set = [set() for _ in range(self.num_product)]
+            s_set[0].add(i)
             ep = 0.0
             for _ in range(self.monte):
-                ep += diffpw_ss.getExpectedProfit(0, i, s_set)
+                ep += diffpw_ss.getSeedSetProfit(s_set)
             ep = round(ep / self.monte, 4)
             mg = round(ep, 4)
+            del s_set
 
-            if mg <= 0:
-                continue
-            for k in range(self.num_product):
-                mg = round(mg * self.product_list[k][0] / self.product_list[0][0], 4)
-                celf_ep = [k, i, mg, 0]
-                celf_seq.append(celf_ep)
-                for celf_item in celf_seq:
-                    if celf_ep[2] >= celf_item[2]:
-                        celf_seq.insert(celf_seq.index(celf_item), celf_ep)
-                        celf_seq.pop()
-                        break
+            if mg > 0:
+                for k in range(self.num_product):
+                    mg = round(mg * self.product_list[k][0] / self.product_list[0][0], 4)
+                    celf_ep = [k, i, mg, 0]
+                    celf_seq.append(celf_ep)
+                    for celf_item in celf_seq:
+                        if celf_ep[2] >= celf_item[2]:
+                            celf_seq.insert(celf_seq.index(celf_item), celf_ep)
+                            celf_seq.pop()
+                            break
 
         return celf_seq
 
@@ -66,6 +52,7 @@ class SeedSelectionNGPW:
 if __name__ == '__main__':
     data_set_name = 'email_undirected'
     product_name = 'r1p3n1'
+    cascade_model = 'ic'
     total_budget = 10
     distribution_type = 1
     whether_passing_information_without_purchasing = bool(0)
@@ -76,22 +63,22 @@ if __name__ == '__main__':
     iniP = IniProduct(product_name)
 
     seed_cost_dict = iniG.constructSeedCostDict()
-    graph_dict = iniG.constructGraphDict()
+    graph_dict = iniG.constructGraphDict(cascade_model)
     product_list = iniP.getProductList()
     num_node = len(seed_cost_dict)
     num_product = len(product_list)
+    product_weight_list = getProductWeight(product_list, distribution_type)
 
     # -- initialization for each budget --
     start_time = time.time()
-    ssngpw = SeedSelectionNGPW(graph_dict, seed_cost_dict, product_list, distribution_type, monte_carlo)
-    pw_list = ssngpw.getProductWeight()
-    diffpw = DiffusionPW(graph_dict, seed_cost_dict, product_list, monte_carlo, pw_list)
+    ssngpw = SeedSelectionNGPW(graph_dict, seed_cost_dict, product_list, product_weight_list, monte_carlo)
+    diffpw = DiffusionPW(graph_dict, seed_cost_dict, product_list, product_weight_list, monte_carlo)
 
     # -- initialization for each sample --
     now_budget, now_profit = 0.0, 0.0
     seed_set = [set() for _ in range(num_product)]
 
-    celf_sequence = ssngpw.generateCelfSequence(pw_list)
+    celf_sequence = ssngpw.generateCelfSequence()
     mep_g = celf_sequence.pop(0)
     mep_k_prod, mep_i_node, mep_flag = mep_g[0], mep_g[1], mep_g[3]
 
@@ -112,15 +99,18 @@ if __name__ == '__main__':
             now_profit = round(ep_g / monte_carlo, 4)
             now_budget = round(now_budget + seed_cost_dict[mep_i_node], 2)
         else:
+            seed_set_t = copy.deepcopy(seed_set)
+            seed_set_t[mep_k_prod].add(mep_i_node)
             ep_g = 0.0
             for _ in range(monte_carlo):
-                ep_g += diffpw.getExpectedProfit(mep_k_prod, mep_i_node, seed_set)
+                ep_g += diffpw.getSeedSetProfit(seed_set_t)
             ep_g = round(ep_g / monte_carlo, 4)
             mg_g = round(ep_g - now_profit, 4)
             ep_flag = seed_set_length
+            del seed_set_t
 
             if mg_g > 0:
-                celf_ep_g = [mep_k_prod, mep_i_node, mg_g, ep_flag]
+                celf_ep_g = (mep_k_prod, mep_i_node, mg_g, ep_flag)
                 celf_sequence.append(celf_ep_g)
                 for celf_item_g in celf_sequence:
                     if celf_ep_g[2] >= celf_item_g[2]:
@@ -131,6 +121,7 @@ if __name__ == '__main__':
         mep_g = celf_sequence.pop(0)
         mep_k_prod, mep_i_node, mep_flag = mep_g[0], mep_g[1], mep_g[3]
 
+    print('seed selection time: ' + str(round(time.time() - start_time, 2)) + 'sec')
     eva = Evaluation(graph_dict, seed_cost_dict, product_list, pp_strategy, whether_passing_information_without_purchasing)
     iniW = IniWallet(data_set_name, product_name, distribution_type)
     wallet_list = iniW.getWalletList()
