3c100600a91b8c7d1a8321a53aab53012d06ebbb
nmaypeter <37822464+nmaypeter@users.noreply.github.com>
2019-04-22 21:37:42 +0800

    Update getFileResultTotalC.py

--- getFileResultTotalC.py
+++ getFileResultTotalC.py
@@ -1,6 +1,10 @@
 data_setting_seq = [1, 2]
 cm_seq = [1, 2]
-model_kinds = 14
+model_seq = ['mngic', 'mpmisic',
+             'mngric', 'mngpwic', 'mngrpwic', 'mngsric', 'mngsrpwic',
+             'mngapic', 'mngapric', 'mngappwic', 'mngaprpwic', 'mngapsric', 'mngapsrpwic',
+             'mhdic', 'mhedic', 'mhdpwic', 'mhedpwic',
+             'mric']
 pps_seq = [1, 2, 3]
 wpiwp_seq = [bool(0), bool(1)]
 prod_setting_seq, prod_setting2_seq = [1, 2], [1, 2, 3]
@@ -18,11 +22,8 @@ for data_setting in data_setting_seq:
                     for prod_setting2 in prod_setting2_seq:
                         product_name = 'r1p3n' + str(prod_setting) + 'a' * (prod_setting2 == 2) + 'b' * (prod_setting2 == 3)
                         for wpiwp in wpiwp_seq:
-                            for m in range(1, model_kinds + 1):
-                                model_name = 'mngic' * (m == 1) + 'mhdic' * (m == 2) + 'mric' * (m == 3) + 'mpmisic' * (m == 4) + \
-                                             'mngric' * (m == 5) + 'mngpwic' * (m == 6) + 'mngrpwic' * (m == 7) + \
-                                             'mhedic' * (m == 8) + 'mhdpwic' * (m == 9) + 'mhedpwic' * (m == 10) + \
-                                             'mngapic' * (m == 11) + 'mngapric' * (m == 12) + 'mngappwic' * (m == 13) + 'mngaprpwic' * (m == 14) + '_pps'
+                            for m in model_seq:
+                                model_name = m + '_pps'
 
                                 try:
                                     result_name = 'result/r_' + data_set_name + '_' + cas_model + '/' + model_name + str(pps) + '_dis' + str(dis) + '_wpiwp' * wpiwp \
@@ -42,7 +43,7 @@ for data_setting in data_setting_seq:
 
                 fw = open('result/r_' + data_set_name + '_' + cas_model + '/dis' + str(dis) + '_pps' + str(pps) + '_comparison_total_profit.txt', 'w')
                 for lnum, line in enumerate(profit):
-                    if lnum % (model_kinds * 2) == 0 and lnum != 0:
+                    if lnum % (len(model_seq) * 2) == 0 and lnum != 0:
                         fw.write('\n' * 3)
                     fw.write(str(line) + '\n')
                 fw.close()
